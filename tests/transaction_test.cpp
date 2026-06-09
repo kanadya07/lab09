0ae73a136df6aa5ba26ef906990502efa26dfbad
kanadya07 <kanadya07@mail.ru>
2026-06-10 00:46:44 +0500

    Fix CI/CD test dependencies

--- tests/transaction_test.cpp
+++ tests/transaction_test.cpp
@@ -1,26 +1,7 @@
 #include "Account.h"
 #include "Transaction.h"
 
-#include <gmock/gmock.h>
 #include <gtest/gtest.h>
-
-using ::testing::_;
-using ::testing::Return;
-
-class MockAccount : public Account {
- public:
-  MockAccount(int id, int balance) : Account(id, balance) {}
-
-  MOCK_METHOD(int, GetBalance, (), (const, override));
-  MOCK_METHOD(void, ChangeBalance, (int), (override));
-  MOCK_METHOD(void, Lock, (), (override));
-  MOCK_METHOD(void, Unlock, (), (override));
-};
-
-class MockTransaction : public Transaction {
- public:
-  MOCK_METHOD(void, SaveToDataBase, (Account& from, Account& to, int sum), (override));
-};
 
 TEST(Transaction, ThrowsOnSameAccount) {
   Transaction transaction;
@@ -51,36 +32,22 @@ TEST(Transaction, ReturnsFalseWhenFeeIsTooLarge) {
 }
 
 TEST(Transaction, ReturnsFalseWhenSourceBalanceIsNotEnough) {
-  MockTransaction transaction;
-  MockAccount from(1, 1000);
-  MockAccount to(2, 500);
-
-  EXPECT_CALL(from, Lock());
-  EXPECT_CALL(to, Lock());
-  EXPECT_CALL(from, Unlock());
-  EXPECT_CALL(to, Unlock());
-  EXPECT_CALL(from, GetBalance()).WillOnce(Return(100));
-  EXPECT_CALL(from, ChangeBalance(_)).Times(0);
-  EXPECT_CALL(to, ChangeBalance(_)).Times(0);
-  EXPECT_CALL(transaction, SaveToDataBase(_, _, _)).Times(0);
+  Transaction transaction;
+  Account from(1, 100);
+  Account to(2, 500);
 
   EXPECT_FALSE(transaction.Make(from, to, 200));
+  EXPECT_EQ(from.GetBalance(), 100);
+  EXPECT_EQ(to.GetBalance(), 500);
 }
 
 TEST(Transaction, TransfersMoneyAndSavesToDatabase) {
-  MockTransaction transaction;
-  MockAccount from(1, 1000);
-  MockAccount to(2, 500);
+  Transaction transaction;
+  Account from(1, 1000);
+  Account to(2, 500);
   transaction.set_fee(50);
 
-  EXPECT_CALL(from, Lock());
-  EXPECT_CALL(to, Lock());
-  EXPECT_CALL(from, Unlock());
-  EXPECT_CALL(to, Unlock());
-  EXPECT_CALL(from, GetBalance()).WillOnce(Return(1000));
-  EXPECT_CALL(from, ChangeBalance(-250));
-  EXPECT_CALL(to, ChangeBalance(200));
-  EXPECT_CALL(transaction, SaveToDataBase(_, _, 200));
-
   EXPECT_TRUE(transaction.Make(from, to, 200));
+  EXPECT_EQ(from.GetBalance(), 750);
+  EXPECT_EQ(to.GetBalance(), 700);
 }
